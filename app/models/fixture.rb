da7e0d91eab3b00f9dd14aa0acbf9484b12c44fd
Graeme <flangemodo@hotmail.com>
2016-01-26 11:02:06 +0000

    notes added

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -2,3 +2,7 @@ class Fixture < ActiveRecord::Base
   belongs_to :home_team, class_name: "FantasyTeam"
   belongs_to :away_team, class_name: "FantasyTeam"
 end
+
+#fantasyleague.fantasyteams[1,2,3,4].combination(2).to_a
+
+# FantasyTeam.ids.combination(2).to_a
