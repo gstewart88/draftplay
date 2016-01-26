77b6f48804620f279b739b5e733c6ec47c35168b
Graeme <flangemodo@hotmail.com>
2016-01-26 11:02:34 +0000

    added scopes

--- app/models/football_player.rb
+++ app/models/football_player.rb
@@ -1,2 +1,9 @@
 class FootballPlayer < ActiveRecord::Base
+  has_many :fantasy_team_players
+
+  scope :random, -> { order('RANDOM()') }
+  scope :goalkeepers, -> { where(position: "Goalkeeper") }
+  scope :defenders, -> { where(position: "Defender") }
+  scope :attackers, -> { where(position: "Attacker") }
+  scope :not_in_league, -> (fantasy_league) { where.not(id: fantasy_league.football_player_ids ) } # TODO: change to use joins rather than nested ids 
 end
